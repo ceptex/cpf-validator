13a9b702d75ecf1f07126813c5421dd541380494
Caio Teixeira <cepteixeira@gmail.com>
2022-05-16 14:23:28 -0300

    created a way to check for know invalid documents

--- script.js
+++ script.js
@@ -18,22 +18,27 @@ function validacao() {
     }
 }
 
-// function sameCharCheck() {
-//     let n = cpf.length;
-//     let cpf = document.getElementById('cpf_digitado').value;
-//     for (let i = 0; i < n; i++) {
-//         if (cpf[i] != cpf[i - 1]) {   
-//             return false;
-//         }
-//     return true;
-//     }
-// }
-
 function validaCPF(cpf) {
     console.log(cpf.length);
     if (cpf.length != 11) {
         return false;
-    } else {
+    } 
+    if (cpf == '') {
+        return false;
+    }
+    if (cpf.length != 11 ||
+        cpf == "00000000000" ||
+        cpf == "11111111111" ||
+        cpf == "22222222222" ||
+        cpf == "33333333333" ||
+        cpf == "44444444444" ||
+        cpf == "55555555555" ||
+        cpf == "66666666666" ||
+        cpf == "77777777777" ||
+        cpf == "88888888888" ||
+        cpf == "99999999999") {
+            return false;
+        } else {
         var numeros = cpf.substring(0, 9);
         var digitos = cpf.substring(9);
         var soma = 0;
